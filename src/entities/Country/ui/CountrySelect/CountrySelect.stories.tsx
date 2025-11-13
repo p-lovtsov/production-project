8746cec51421ff00b969c7078015c0956fa9dc97
Pavel Lovtsov <p.lovtsov@gmail.com>
2025-11-13 20:34:12 +0300

    add CountrySelect story

---
+++ src/entities/Country/ui/CountrySelect/CountrySelect.stories.tsx
@@ -0,0 +1,18 @@
+import { type ComponentStory, type ComponentMeta } from '@storybook/react';
+
+import { CountrySelect } from './CountrySelect';
+import 'app/styles/index.scss';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: 'entities/CountrySelect',
+  component: CountrySelect,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof CountrySelect>;
+
+const Template: ComponentStory<typeof CountrySelect> = (args) => <CountrySelect {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
